4ffd6ce9a8cad32d53b41824d7e320e40b2d69d5
Luis Recio <recluising@gmail.com>
2017-05-07 20:01:59 +0200

    test(test/, virtual-chat.html): some unit tests added

---
+++ test/mockScript.js
@@ -0,0 +1,38 @@
+var mockHamlet = [
+  {
+    "message": "ACT I",
+    "user": "Director"
+  },
+  {
+    "message": "SCENE I. Elsinore. A platform before the castle.",
+    "user": "Director"
+  },
+  {
+    "message": "Who's there?",
+    "user": "BERNARDO"
+  },
+  {
+    "message": "Nay, answer me: stand, and unfold yourself.",
+    "user": "FRANCISCO"
+  },
+  {
+    "message": "Long live the king!",
+    "user": "BERNARDO"
+  },
+  {
+    "message": "Bernardo?",
+    "user": "FRANCISCO"
+  },
+  {
+    "message": "He.",
+    "user": "BERNARDO"
+  },
+  {
+    "message": "You come most carefully upon your hour.",
+    "user": "FRANCISCO"
+  },
+  {
+    "message": "'Tis now struck twelve; get thee to bed, Francisco.",
+    "user": "BERNARDO"
+  }
+];
